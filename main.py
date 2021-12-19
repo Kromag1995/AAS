f4278296e557898bb1c63e312f1ffef21806934a
Kromag1995 <gonzalof95@hotmail.com>
2021-12-19 16:31:15 -0300

    Add seleccion_de_procesos

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import click
 
+def seleccion_de_procesos():
+    """
+    Funcion para seleccionar que procesos correr
+    """
+    procesos_disponibles = ["Filtrado de datos", "Deteccion de maximos", "Binarizacion por estadistica",
+                            "Test de aleatoriedad", "Test de caoticidad", "Filtrar maximos segun salto"]
+    procesos_seleccionados = []
+    t = "".join(f"[{i}] {p}\n" for i,p in enumerate(procesos_disponibles))
+    procesos_seleccionados.append(
+        click.prompt(f"seleccione que proceso correr\n{t}")
+    )
+    t += f"[R] Imprimir esta lista\n"
+    while True:
+        p = click.prompt(f"Seleccione el siguiente proceso, de no haber mas entre [Q]\n{t}").upper()
+        if p == "Q":
+            break
+        procesos_seleccionados.append(p)
+    return procesos_seleccionados
+
+
 @click.command()
-@click.option('--process', promt="Bienvenido a AAS, seleccione que proceso correr")
 def main():
-    print("")
-    print("1) Filtrado de Datos")
-    print("2) Filtrado de Datos")
-    print("1)")
-    print("1)")
+    print("Bienvenido a AAS")
+    tareas_a_procesar = seleccion_de_procesos()
+    print(tareas_a_procesar)
 
 if __name__ == '__main__':
     main()
